cffcb1f2e9a6ee0ccc771d0e8e469e0d3bbb1ea8
Keven Mateus <90327028+KevenMateus@users.noreply.github.com>
2021-10-20 21:55:37 -0300

    Add files via upload

---
+++ Enviar FixPay.py
@@ -0,0 +1,23 @@
+import smtplib
+import email.message
+
+def enviar_email():  
+    corpo_email = """
+    <p>Parágrafo1</p>
+    <p>Parágrafo2</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Assunto"
+    msg['From'] = 'remetente'
+    msg['To'] = 'destinatario'
+    password = 'senha' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(corpo_email )
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
